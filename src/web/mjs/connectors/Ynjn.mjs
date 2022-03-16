e9537e8248672cfd3e6e7142e90df49cd4d128ca
gammesB <gammes.bubble@gmail.com>
2022-03-16 15:08:21 +0100

    ynjn

---
+++ src/web/mjs/connectors/Ynjn.mjs
@@ -0,0 +1,39 @@
+import SpeedBinb from './templates/SpeedBinb.mjs';
+import Manga from '../engine/Manga.mjs';
+
+export default class Ynjn extends SpeedBinb {
+
+    constructor() {
+        super();
+        super.id = 'ynjn';
+        super.label = 'ヤンジャン！ (ynjn)';
+        this.tags = ['manga', 'japanese'];
+        this.url = 'https://ynjn.jp';
+    }
+
+    async _getMangaFromURI(uri) {
+        let request = new Request(uri, this.requestOptions);
+        let data = await this.fetchDOM(request, '.comic_title');
+        let id = uri.pathname;
+        let title = data[0].innerText.trim();
+        return new Manga(this, id, title);
+    }
+
+    async _getMangas() {
+        let msg = "This website doesn't provides a manga list, please copy and paste the URL containing the images directly from your browser into HakuNeko.";
+        throw new Error(msg);
+    }
+    async _getChapters(manga) {
+        const uri = new URL(manga.id, this.url);
+        const request = new Request(uri, this.requestOptions);
+        const data = await this.fetchDOM(request, '.episodes_area .episode:not(.disabled) a');
+        return data.reverse()
+            .filter(e => e.querySelector(".button .button-box").getAttribute("class").includes("btn_free"))
+            .map(element => {
+                return {
+                    id:  this.getRootRelativeOrAbsoluteLink(element, this.url),
+                    title: element.querySelector('.num').textContent.trim() + " " + element.querySelector('.title').textContent.trim()
+                };
+            });
+    }
+}
